bc4d0b3bd3d4204212dfc0215527769e8bd39a88
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2023-05-23 10:00:23 -0500

    remove file

--- exception/provider.py
+++
@@ -1,8 +0,0 @@
-from . import Error, TypeMessage, Validate
-
-
-class ProviderValidate(Exception):
-    def __init__(self, message: Validate) -> None:
-        self.type = TypeMessage.VALIDATION.name
-        self.message = message.value
-        super().__init__(self.message)
