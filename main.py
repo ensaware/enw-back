18f07b7119d538f7943c03570d27116ac6839580
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2023-05-23 18:23:12 -0500

    add exception handler

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 
+from exception.ensaware import EnsawareException, EnsawareExceptionHandler
 from qr_code.v1.router import router as qr_code
 from user.v1.router import router as user
 from utils.settings import Settings
 
 
 settings = Settings()
+ensaware_exception_handler = EnsawareExceptionHandler()
 
 
 if settings.debug == 1:
@@ -18,6 +20,10 @@ app = FastAPI(
     version='0.0.1',
 )
 
+
+app.exception_handler(EnsawareException)(ensaware_exception_handler.ensaware)
+
+
 app.include_router(
     qr_code,
     prefix='/v1/qr-code',
@@ -26,6 +32,6 @@ app.include_router(
 
 app.include_router(
     user,
-     prefix='/v1/user',
+    prefix='/v1/user',
     tags=['V1 - User']
 )
