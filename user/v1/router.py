99fb98e8ceb66de104452a4d69955f4f1bc58013
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2023-05-23 18:30:47 -0500

    add documentation

--- user/v1/router.py
+++ user/v1/router.py
@@ -1,19 +1,18 @@
-from fastapi import APIRouter, Depends, Header, status, Request, Response, UploadFile
+import logging
+
+from fastapi import APIRouter, Depends, status, Request
 from fastapi.responses import RedirectResponse
-from fastapi.security import HTTPBearer
 from sqlalchemy.orm import Session
-from typing import Annotated
 
 from auth.provider import Provider, SelectProvider
+from exception.ensaware import EnsawareException, EnsawareExceptionBase
 from utils.database import ENGINE, get_db
 
-from . import crud, JWT, models, schema
+from . import crud, models, schema, DecryptedToken
 
 
 router = APIRouter()
 models.Base.metadata.create_all(bind=ENGINE)
-oauth2_token = HTTPBearer()
-jwt = JWT()
 
 
 @router.get(
@@ -24,6 +23,15 @@ def login_provider(
     request: Request,
     provider: Provider,
 ):
+    '''
+    Permite iniciar sessión a través de OAuth.
+
+    - `provider` Nombre del proveedor.
+
+    Redirecciona a la url del proveedor para pedir autorización al usuario.
+
+    **Importante:** Por el momento solo se tiene el proveedor Google.
+    '''
     url: str = f'{str(request.url)}/auth'
     redirect_url, _ = SelectProvider.select(provider, url).authentication()
 
@@ -38,29 +46,74 @@ def login_provider(
 def login_provider(
     request: Request,
     provider: Provider,
-    code: str,
     db: Session = Depends(get_db)
 ):
+    '''
+    callback URL con la respuesta del proveedor.
+
+    - `provider` Nombre del proveedor.
+
+    ### Return
+    - `Token class` Respuesta del proveedor donde se entrege el token, token_type y refresh_token.
+
+    **Importante:** Por el momento solo se tiene el proveedor Google.
+    '''
     index: int = str(request.url).index('?')
     url: str = str(request.url)[0: index]
     token: schema.Token = SelectProvider.select(provider, url).get_data(db, request)
 
-    return RedirectResponse(
-        url = '/v1/user/me',
-         headers = {
-            'Authorization': f'{token.token_type} {token.token}'
-        }
-    )
+    return token
 
 
 @router.get(
     '/me',
+    responses={
+        401: {
+            'model': EnsawareExceptionBase
+        }
+    },
     response_model=schema.User,
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
 )
 def user_me(
-    token: Annotated[str, Depends(oauth2_token)],
+    token: schema.TokenData = Depends(DecryptedToken.get_token),
     db: Session = Depends(get_db)
 ):
-    token_data: schema.TokenData = jwt.decode(token.credentials)
-    return crud.get_user_id(db, token_data.sub)
+    '''
+    Entrega información del usuario.
+
+    **Nota:** Se debe de enviar en el encabezado el token del usuario.
+    '''
+    try:
+        return crud.get_user_id(db, token.sub)
+    except EnsawareException as enw:
+        logging.exception(enw)
+        raise enw
+    
+
+@router.post(
+    '/refresh/token/{provider}',
+    responses={
+        400: {
+            'model': EnsawareExceptionBase
+        }
+    },
+    response_model=schema.Token,
+    status_code=status.HTTP_200_OK,
+)
+def refresh_token(
+    provider: Provider,
+    refresh_token: schema.RefreshToken,
+    db: Session = Depends(get_db)
+):
+    '''
+    Actualizar token vencido.
+
+    ### Return
+    - `Token class` Respuesta del proveedor donde se entrege el token, token_type y refresh_token.
+    '''
+    try:
+        return SelectProvider.select(provider, '').refresh_token(db, refresh_token.refresh_token)
+    except EnsawareException as enw:
+        logging.exception(enw)
+        raise enw
