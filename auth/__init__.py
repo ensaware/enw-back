3771247ca803b8874318ccb8d06af94c430300b1
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2023-06-18 16:19:44 -0500

    class rename

--- auth/__init__.py
+++ auth/__init__.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 from utils.encryption import Encryption
 
 
-class Auth20(ABC):
+class OAuth20(ABC):
     def __init__(self, url_callback: str) -> None:
         self.url_callback: str = url_callback
         self.encryption: Encryption = Encryption()
